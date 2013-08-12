252f3bf9e881bdd51dad9c4d59fc36d8ef190bd3
Marc Sune <marc.sune@bisdn.de>
2013-08-12 10:48:32 +0200

    Defining key methods of datapacketx86 as inline

--- src/xdpd/fwd-modules/gnu_linux/src/io/datapacketx86.h
+++ src/xdpd/fwd-modules/gnu_linux/src/io/datapacketx86.h
@@ -65,7 +65,7 @@ public:
 public: // methods
 
 	//Initialize the already constructed object
-	rofl_result_t init(uint8_t* buf, size_t buflen, of_switch_t* sw, uint32_t in_port, uint32_t in_phy_port = 0, bool classify=true, bool copy_packet_to_internal_buffer = true);
+	inline rofl_result_t init(uint8_t* buf, size_t buflen, of_switch_t* sw, uint32_t in_port, uint32_t in_phy_port = 0, bool classify=true, bool copy_packet_to_internal_buffer = true);
 
 	//Destroy object. This is NOT a destructor nor releases memory, but resets fields
 	void destroy(void);
@@ -121,19 +121,18 @@ private:
 	 * utility function to set the correct buffer location
 	 * @param location
 	 */
-	void
-	init_internal_buffer_location_defaults(x86buffering_status_t location, uint8_t* buf, size_t buflen);
+	inline void init_internal_buffer_location_defaults(x86buffering_status_t location, uint8_t* buf, size_t buflen);
 	//Add more stuff here...
 
 	
 	/*
 	* Push&pop raw operations. To be used ONLY by classifiers
 	*/
-	rofl_result_t push(unsigned int num_of_bytes, unsigned int offset = 0);
-	rofl_result_t pop(unsigned int num_of_bytes, unsigned int offset = 0);
+	inline rofl_result_t push(unsigned int num_of_bytes, unsigned int offset = 0);
+	inline rofl_result_t pop(unsigned int num_of_bytes, unsigned int offset = 0);
 
-	rofl_result_t push(uint8_t* push_point, unsigned int num_of_bytes);
-	rofl_result_t pop(uint8_t* pop_point, unsigned int num_of_bytes);
+	inline rofl_result_t push(uint8_t* push_point, unsigned int num_of_bytes);
+	inline rofl_result_t pop(uint8_t* pop_point, unsigned int num_of_bytes);
 
 };
 
@@ -141,7 +140,59 @@ private:
 * Inline functions
 */ 
 
-inline void
+//Init
+rofl_result_t
+datapacketx86::init(
+		uint8_t* buf, size_t buflen,
+		of_switch_t* sw,
+		uint32_t in_port,
+		uint32_t in_phy_port,
+		bool classify, 
+		bool copy_packet_to_internal_buffer)
+{
+	// do this sanity check here, as someone may request later a transfer to user space,
+	// so make sure we have enough space for doing this later
+	if (buflen > FRAME_SIZE_BYTES){
+		return ROFL_FAILURE;
+	}
+#if 0
+	// if buffer is NULL we initialize to local buffer and do not copy anything
+	if (NULL == buf) {
+		init_internal_buffer_location_defaults(X86_DATAPACKET_BUFFERED_IN_USER_SPACE, NULL, buflen);
+		return ROFL_SUCCESS;
+	}
+#endif
+
+	if( copy_packet_to_internal_buffer) {
+
+		init_internal_buffer_location_defaults(X86_DATAPACKET_BUFFERED_IN_USER_SPACE, NULL, buflen);
+
+		if(buf)
+			platform_memcpy(buffer.iov_base, buf, buflen);
+	}else{
+		if(!buf)
+			return ROFL_FAILURE;
+
+		init_internal_buffer_location_defaults(X86_DATAPACKET_BUFFERED_IN_NIC, buf, buflen);
+	}
+
+	//Fill in
+	this->lsw = sw;
+	this->in_port = in_port;
+	this->in_phy_port = in_phy_port;
+	//this->eth_type 		= 0;
+
+	this->output_queue = 0;
+
+	//Classify the packet
+	if(classify)
+		headers->classify();
+
+	return ROFL_SUCCESS;
+}
+
+
+void
 datapacketx86::init_internal_buffer_location_defaults(
 		x86buffering_status_t location, uint8_t* buf, size_t buflen)
 {
@@ -173,6 +224,145 @@ datapacketx86::init_internal_buffer_location_defaults(
 			// todo ?
 			break;
 	}
+}
+
+
+
+/*
+ * Push&pop operations
+ */
+rofl_result_t
+datapacketx86::push(
+		unsigned int offset,
+		unsigned int num_of_bytes)
+{
+	//If not already transfer to user space
+	if(X86_DATAPACKET_BUFFERED_IN_NIC == buffering_status){
+		transfer_to_user_space();
+	}
+	
+	if (offset > buffer.iov_len){
+		return ROFL_FAILURE;
+	}
+
+	size_t free_space_head = (uint8_t*)buffer.iov_base - (uint8_t*)slot.iov_base;
+	size_t free_space_tail = slot.iov_len - (free_space_head + buffer.iov_len);
+
+	/*
+	 * this is the safe sanity check for both head and tail space
+	 */
+	if (num_of_bytes > (free_space_head + free_space_tail)){
+		return ROFL_FAILURE;
+	}
+
+	/*
+	 * implicit assumption: we only have pre-guard-bytes and move forward and backward at the head of the packet
+	 */
+	if (num_of_bytes > free_space_head){
+		return ROFL_FAILURE;
+	}
+
+	// move header num_of_bytes backward
+	platform_memmove((uint8_t*)buffer.iov_base - num_of_bytes, buffer.iov_base, offset);
+#ifndef NDEBUG
+	// initialize new pushed memory area with 0x00
+	platform_memset((uint8_t*)buffer.iov_base - num_of_bytes + offset, 0x00, num_of_bytes);
+#endif
+
+	buffer.iov_base = (uint8_t*)buffer.iov_base - num_of_bytes;
+	buffer.iov_len += num_of_bytes;
+
+
+	return ROFL_SUCCESS;
+}
+
+
+
+rofl_result_t
+datapacketx86::pop(
+		unsigned int offset,
+		unsigned int num_of_bytes)
+{
+	//Check boundaries
+	//FIXME
+
+	//If not already transfer to user space
+	if(X86_DATAPACKET_BUFFERED_IN_NIC == buffering_status){
+		transfer_to_user_space();
+	}
+
+	// sanity check: start of area to be deleted must not be before start of buffer
+	if (offset > buffer.iov_len){
+		return ROFL_FAILURE;
+	}
+
+	// sanity check: end of area to be deleted must not be behind end of buffer
+	if ((offset + num_of_bytes) > buffer.iov_len){
+		return ROFL_FAILURE;
+	}
+
+	// move first bytes backward
+	platform_memmove((uint8_t*)buffer.iov_base + num_of_bytes, buffer.iov_base, offset);
+
+#ifndef NDEBUG
+	// set now unused bytes to 0x00 for easier debugging
+	platform_memset(buffer.iov_base, 0x00, num_of_bytes);
+#endif
+
+	buffer.iov_base = (uint8_t*)buffer.iov_base + num_of_bytes;
+	buffer.iov_len -= num_of_bytes;
+
+	// re-parse_ether() here? yes, we have to, but what about the costs?
+
+	return ROFL_SUCCESS;
+}
+
+
+
+//Push&pop operations
+rofl_result_t
+datapacketx86::push(
+		uint8_t* push_point,
+		unsigned int num_of_bytes)
+{
+	//If not already transfer to user space
+	if(X86_DATAPACKET_BUFFERED_IN_NIC == buffering_status){
+		transfer_to_user_space();
+	}
+	
+	if (push_point < buffer.iov_base){
+		return ROFL_FAILURE;
+	}
+
+	if (((uint8_t*)push_point + num_of_bytes) > ((uint8_t*)buffer.iov_base + buffer.iov_len)){
+		return ROFL_FAILURE;
+	}
+
+	//size_t offset = ((uint8_t*)buffer.iov_base - push_point);
+	size_t offset = (push_point - (uint8_t*)buffer.iov_base);
+
+	return push(offset, num_of_bytes);
+}
+
+
+
+rofl_result_t
+datapacketx86::pop(
+		uint8_t* pop_point,
+		unsigned int num_of_bytes)
+{
+	if (pop_point < buffer.iov_base){
+		return ROFL_FAILURE;
+	}
+
+	if (((uint8_t*)pop_point + num_of_bytes) > ((uint8_t*)buffer.iov_base + buffer.iov_len)){
+		return ROFL_FAILURE;
+	}
+
+	//size_t offset = ((uint8_t*)buffer.iov_base - pop_point);
+	size_t offset = ((uint8_t*)buffer.iov_base - pop_point);
+
+	return pop(offset, num_of_bytes);
 }
 
 
